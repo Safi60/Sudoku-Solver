12a7823192509f5bf954d85671a9017ffe6e908c
Safi <safi.hanifa06@gmail.com>
2021-12-24 18:32:05 +0100

    sudoku solver

---
+++ server.js
@@ -0,0 +1,32 @@
+const PORT = 8000;
+const axios = require('axios').default;
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+app.post('/solve', (req,res) => {
+    const options = {
+        method: 'POST',
+        url: 'https://solve-sudoku.p.rapidapi.com/',
+        headers: {
+            'content-type': 'application/json',
+            'x-rapidapi-host': 'solve-sudoku.p.rapidapi.com',
+            'x-rapidapi-key':  process.env.RAPID_API_KEY
+        },
+        data: {
+            puzzle: req.body.numbers
+        }
+    };
+    axios.request(options).then((response) => {
+        console.log(response.data);
+        //populateValues(response.data.solvable, response.data.solution)
+        res.json(response.data)
+    }).catch((error) => {
+        console.error(error);
+    });
+})
+
+app.listen(PORT, () => console.log(`server listening on PORT ${PORT}`));
